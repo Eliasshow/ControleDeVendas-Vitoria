a97b96484b15d9ec2a8e44f41e9b0112a8d33a14
Eliasshow <eliassamuel395@gmail.com>
2026-07-07 13:05:40 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -19,11 +19,13 @@ export default defineConfig({
             src: '/logo.png',
             sizes: '192x192',
             type: 'image/png'
+            purpose: 'any maskable'
           },
           {
             src: '/logo.png',
             sizes: '512x512',
             type: 'image/png'
+            purpose: 'any maskable'
           }
         ]
       }
